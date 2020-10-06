ee798ecf8ff9fc5509288e130944f03001a16e5a
mac <mac@macs-MacBook-Pro-2.local>
2020-10-06 16:29:15 +0100

    Creating Navigation using the Angular Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { Routes } from "@angular/router";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
