8b3c84fc7882d4823ad32ada4bd66c1226c71dee
mac <mac@macs-MacBook-Pro-2.local>
2020-10-06 15:43:58 +0100

    list of products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,12 +3,12 @@ import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import "rxjs/add/operator/map";
 import { Album } from "./album";
-import {Product} from "./product";
+import { Product } from "./product";
 
 @Injectable()
 export class ProductService {
   private _albumUrl: string = "../assets/album.json";
-  private _productsUrl: string "../assets/products.json";
+  private _productsUrl: string = "../assets/products.json";
 
   constructor(private _http: Http) {}
 
@@ -18,7 +18,7 @@ export class ProductService {
       .map((response) => <Album>response.json());
   }
 
-  getProducts(
-    return this._http.get(_productsUrl).map(response => response.json)
-  );
+  getProducts() {
+    return this._http.get(_productsUrl).map((response) => response.json);
+  }
 }
